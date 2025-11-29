eeee2e31caa8c108e117738bfeca5c4e9c5ecf92
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-11-29 22:43:25 +0300

    main app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
+import { Metadata } from "next";
+
+//-- needs to be exported so that the metadata can be rendered in the web page
+export const metadata: Metadata = {
+  title: "RESTapi",
+  description: "Creating rest api with Next.js",
+};
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <body>
         <div className="min-h-screen bg-gray-100">
-          <nav className="bg-white shadow-sm p-4">
-            <h1 className="text-xl font-bold">Dashboard Layout</h1>
-          </nav>
           <div className="container mx-auto p-4">{children}</div>
         </div>
       </body>
