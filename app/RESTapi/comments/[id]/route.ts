e89ca09d80e7c4dd6a89f755c65f3e906093b173
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-11-29 23:05:20 +0300

    patch request handler

--- app/RESTapi/comments/[id]/route.ts
+++ app/RESTapi/comments/[id]/route.ts
@@ -8,3 +8,21 @@ export const GET = async (
   const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 };
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  // detructure id form awaited Promise
+  const { id } = await params;
+
+  // detructure body content from incoming request body request
+
+  // const body = awa req.json()
+  // const comment = body.comment
+  const { comment } = await req.json();
+
+  const index = comments.findIndex((c) => c.id === parseInt(id));
+  comments[index].comment = comment;
+  return Response.json(comments[index]);
+};
