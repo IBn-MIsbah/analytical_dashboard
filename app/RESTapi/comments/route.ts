82db9d2ad3faf7294c18fe9c9ba58819cb5523fb
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-11-30 09:43:30 +0300

    using query param

--- app/RESTapi/comments/route.ts
+++ app/RESTapi/comments/route.ts
@@ -1,8 +1,9 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export const GET = async () => {
-  return Response.json(comments);
-};
+// export const GET = async () => {
+//   return Response.json(comments);
+// };
 
 export const POST = async (req: Request) => {
   const incomming = await req.json();
@@ -14,5 +15,22 @@ export const POST = async (req: Request) => {
   return new Response(JSON.stringify(newComment), {
     headers: { "Content-Type": "application/json" },
     status: 201,
+  });
+};
+
+export const GET = async (req: NextRequest) => {
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filterComments = query
+    ? comments.filter((c) =>
+        c.comment.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      )
+    : comments;
+
+  return Response.json({
+    data: filterComments,
+    total: filterComments.length,
+    hasQuery: !!query,
+    query: query || null,
   });
 };
